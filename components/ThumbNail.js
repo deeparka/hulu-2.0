a3227c64f02a980ef5950aa6a70d5cf74abaca81
deeparka <arkadeep180@gmail.com>
2022-11-07 13:52:36 +0530

    completed the application

--- components/ThumbNail.js
+++ components/ThumbNail.js
@@ -1,11 +1,14 @@
+/* eslint-disable react/display-name */
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
+import { forwardRef } from "react";
 
-function ThumbNail({ result }) {
+const ThumbNail = forwardRef(({ result }, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
     return (
         <div
+            ref={ref}
             className="group cursor-pointer p-2 transition
         duration-200 ease-in transform sm:hover:scale-105
         hover:z-50"
@@ -31,16 +34,17 @@ function ThumbNail({ result }) {
                     {result.title || result.original_name}
                 </h2>
                 <p
-                    className="flex items-center opacity-0 
-                group-hover:opacity-100"
+                    className="flex items-center md:opacity-0 
+                md:group-hover:opacity-100 sm:opacity-100"
                 >
-                    {result.media_type && `${result.media_type}`}
+                    {result.media_type && `${result.media_type} -- `}
                     {result.release_date || result.first_air_date} --
-                    <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}{" "}
+                    <ThumbUpIcon className="h-5 mx-2" />
+                    {result.vote_count}{" "}
                 </p>
             </div>
         </div>
     );
-}
+});
 
 export default ThumbNail;
