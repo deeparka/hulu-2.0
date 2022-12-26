023ade5be49516683119a1ffee72d6b3e9821295
deeparka <arkadeep180@gmail.com>
2022-12-26 22:38:10 +0530

    initial commit

--- components/Results.js
+++ components/Results.js
@@ -2,16 +2,16 @@ import ThumbNail from "./ThumbNail";
 import FlipMove from "react-flip-move";
 
 function Results({ results }) {
-    return (
-        <FlipMove
-            className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3
+  return (
+    <FlipMove
+      className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3
         3xl:grid-cols-5"
-        >
-            {results.map((result) => (
-                <ThumbNail key={result.id} result={result} />
-            ))}
-        </FlipMove>
-    );
+    >
+      {results.map((result) => (
+        <ThumbNail key={result.id} result={result} />
+      ))}
+    </FlipMove>
+  );
 }
 
 export default Results;
